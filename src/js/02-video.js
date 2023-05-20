545e7e1fd81dc41c9b9b7328047b064c9f7bc16c
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-20 21:11:59 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+const KEY_STORAGE = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(KEY_STORAGE);
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+player.on(
+  'timeupdate',
+  throttle(() => {
+    const currentTime = player.getCurrentTime();
+    localStorage.setItem(KEY_STORAGE, currentTime);
+  }, 1000)
+);
