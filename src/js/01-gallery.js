66c47a16569fb2a3c5687b1b12dd4856ae3556f4
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-21 14:16:45 +0300

    fix 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,35 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const galleryRef = document.querySelector('.gallery');
-const createGalleryItem = ({ preview, original, description }) => {
-  return `  
-     <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
-};
-const makeGalleryItemMarup = galleryItems.map(createGalleryItem).join('');
-galleryRef.insertAdjacentHTML('beforeend', makeGalleryItemMarup);
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
+
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+function renderGalleryItems() {
+  const galleryMarkup = galleryItems
+    .map(item => createGalleryItemMarkup(item))
+    .join('');
+  galleryContainer.innerHTML = galleryMarkup;
+}
+renderGalleryItems();
+const gallery = new SimpleLightbox('.gallery__item a', {
+  captions: true,
   captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'top',
 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,43 +1,25 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.getElementById('vimeo-player');
-const player = new Player(iframe);
+const player = new Player('vimeo-player');
 
-const THROTTLE_DELAY = 1000; // 1 second
-const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const saveCurrentTime = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
 
-// Отримання збереженого часу відтворення з локального сховища
-function getSavedTime() {
-  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
-  return savedTime ? parseFloat(savedTime) : 0;
+const getCurrentTime = () => {
+  return localStorage.getItem('videoplayer-current-time');
+};
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    const currentTime = event.seconds;
+    saveCurrentTime(currentTime);
+  }, 1000)
+);
+
+const storedTime = getCurrentTime();
+if (storedTime) {
+  player.setCurrentTime(storedTime);
 }
-
-// Збереження поточного часу відтворення у локальне сховище
-function saveTime(time) {
-  localStorage.setItem(LOCAL_STORAGE_KEY, time.toString());
-}
-
-// Встановлення часу відтворення після завантаження сторінки
-function restoreTime() {
-  const savedTime = getSavedTime();
-  player.setCurrentTime(savedTime).catch(error => {
-    if (error.name === 'RangeError') {
-      // Обробка помилки, якщо збережений час виходить за межі відео
-      console.error('Invalid saved time:', savedTime);
-    } else {
-      console.error('Error restoring time:', error);
-    }
-  });
-}
-
-// Відстежування події оновлення часу відтворення з певною затримкою
-const throttledSaveTime = throttle(time => {
-  saveTime(time);
-}, THROTTLE_DELAY);
-
-// Оновлення часу відтворення при зміні
-player.on('timeupdate', event => {
-  const currentTime = event.seconds;
-  throttledSaveTime(currentTime);
-});
